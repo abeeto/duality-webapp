c1801bab6efe6db5897e21c8b1b8203e811a8077
Abhinav Thota <abhinavthota01@gmail.com>
2024-03-02 19:22:45 -0800

    set up skeleton way of entering chat with user

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -3,12 +3,27 @@ import Message from "./Message";
 import SendMessage from "./SendMessage";
 
 const ChatBox = () => {
+  const [room, setRoom] = React.useState(false);
+  const roomInputRef = React.useRef(null);
   return (
     <main className="chat-box">
+      {room? 
+      (<>
       <div className="messages-wrapper">
         <Message />
       </div>
       <SendMessage />
+      </>
+      ) : (
+      <>
+        <div className="no-room">
+          <h2>Welcome to Chat</h2>
+          <p>Select a room to start chatting</p>
+          <input type="text" placeholder="Enter username to chat with" ref={roomInputRef}/>
+          <button onClick={() => setRoom(roomInputRef.current.value)}>Talk with user</button>
+        </div>
+      </>
+      )}
     </main>
   );
 };
